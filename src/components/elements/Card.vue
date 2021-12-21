b166b1a6db4307ef783a47b8421c0eee5f3a5d68
Francesco <scovascova@gmail.com>
2021-12-21 20:34:44 +0100

    dati inseriti in una card

---
+++ src/components/elements/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>{{info.title}}</h3>
+        <h4>{{info.original_title}}</h4>
+        <p>{{info.original_language}}</p>
+        <p>{{info.vote_average}}</p>
+    </div>
+     
+</template>
+
+<script>
+export default {
+    name:'Card',
+    props: {
+        info: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/sections/FilmList.vue
+++ src/components/sections/FilmList.vue
@@ -2,21 +2,21 @@
   <section>
       <ul>
           <li v-for="(film, index) in films" :key="index">
-              <h3>{{film.title}}</h3>
-              <h4>{{film.original_title}}</h4>
-              <p>{{film.original_language}}</p>
-              <p>{{film.vote_average}}</p>
-
+              <Card :info="film"/>
           </li>
       </ul>
   </section>
 </template>
 
 <script>
+import Card from '../elements/Card.vue';
 export default {
-    name: 'FilmList',
-    props: {
+  name: 'FilmList',
+  props: {
       films: Array
+  },
+  components: {
+      Card
   }
 }
 </script>
